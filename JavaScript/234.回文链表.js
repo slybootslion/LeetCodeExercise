e895494e3152ee5d301c55ecaaea3a8a2edbb243
slybootslion <slybootslion@vip.qq.com>
2021-06-30 14:43:54 +0800

    2021-06-30 14:43:54

--- JavaScript/234.回文链表.js
+++ JavaScript/234.回文链表.js
@@ -26,6 +26,7 @@ Accepted
 Your runtime beats 25.87 % of javascript submissions
 Your memory usage beats 46.38 % of javascript submissions (64.4 MB)
 */
+/* 
 var isPalindrome = function (head) {
   let p = head
   const arr = []
@@ -40,5 +41,81 @@ var isPalindrome = function (head) {
   }
   return true
 };
+*/
+
+/* 
+方法二：反转链表再比对
+是的，你没有看错leetcode可以用lodash。不让用就自己写个深拷贝
+这种方法性能感人
+*/
+/* 
+Accepted
+85/85 cases passed (3240 ms)
+Your runtime beats 5 % of javascript submissions
+Your memory usage beats 5.05 % of javascript submissions (97.4 MB)
+*/
+/* 
+const isPalindrome = head => {
+  const origin = _.cloneDeep(head)
+  const revers = head => {
+    let p = head
+    let l = null
+    while (p) {
+      let temp = p.next
+      p.next = l
+      l = p
+      p = temp
+    }
+    return l
+  }
+  const reLink = revers(head)
+  let p = origin
+  let l = reLink
+  while (l && p) {
+    console.log(l.val, p.val)
+    if (l.val !== p.val) return false
+    l = l.next
+    p = p.next
+  }
+  return true
+}
+*/
+
+/* 
+方法三：倒序遍历
+贴个伪代码，关于遍历顺序的写法（递归版）：
+二叉树↓
+void traverse(TreeNode root) {
+    前序遍历代码
+    traverse(root.left);
+    中序遍历代码
+    traverse(root.right);
+    后序遍历代码
+}
+链表↓
+void traverse(ListNode head) {
+    前序遍历代码
+    traverse(head.next);
+    后序遍历代码
+}
+*/
+/* 
+Accepted
+85/85 cases passed (248 ms)
+Your runtime beats 35.52 % of javascript submissions
+Your memory usage beats 5.05 % of javascript submissions (71.8 MB)
+*/
+
+const isPalindrome = head => {
+  let left = head
+  const traverse = right => {
+    if (!right) return true
+    let res = traverse(right.next)
+    res = res && right.val === left.val
+    left = left.next
+    return res
+  }
+  return traverse(head)
+}
 // @lc code=end
 
