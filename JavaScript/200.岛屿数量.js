bee210abb4bd390ee120eda7b843341aa891d1df
slybootslion <slybootslion@vip.qq.com>
2021-07-07 08:44:16 +0800

    2021-07-07 08:44:16

--- JavaScript/200.岛屿数量.js
+++ JavaScript/200.岛屿数量.js
@@ -18,30 +18,29 @@ Accepted
 Your runtime beats 36 % of javascript submissions
 Your memory usage beats 28.56 % of javascript submissions (40.8 MB)
 */
-var numIslands = function (grid) {
-  if (!grid.length) return 0
-  let queue = []
+var numIslands = function(grid) {
+  const len = grid.length
+  if (!len) return
   let count = 0
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] === '1') {
+  let queue = []
+  const dists = [[0,1], [1,0], [0, -1], [-1, 0]]
+  const c = grid[0].length
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < c; j++) {
+      if(grid[i][j] === '1') {
         count++
-        grid[i][j] = 0
-        queue.push([i, j]) // 推入要遍历的队列
-        const dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        // 循环，直到队列清空
-        while (queue.length) { 
-          const current = queue.shift()
-          for (let i = 0; i < dirs.length; i++) {
-            const dir = dirs[i]
-            const x = current[0] + dir[0]
-            const y = current[1] + dir[1]
-            if (x < 0 || x >= grid.length || y < 0 || y > grid[0].length || grid[x][y] !== '1') {
-              continue
+        queue.push([i, j])
+        grid[i][j] = '0'
+        while(queue.length){
+          const cur = queue.shift()
+          dists.forEach(dist => {
+            const x = cur[0] + dist[0]
+            const y = cur[1] + dist[1]
+            if (grid[x] && grid[x][y] === '1') {
+               grid[x][y] = '0'
+               queue.push([x, y])
             }
-            grid[x][y] = '0'
-            queue.push([x, y])
-          }
+          })
         }
       }
     }
