bf85a80fe8aa80a9409afeb2b76c152258e94917
slybootslion <slybootslion@vip.qq.com>
2021-09-22 10:23:58 +0800

    2021-09-22 10:23:58

--- Python3/912.排序数组.py
+++ Python3/912.排序数组.py
@@ -29,7 +29,7 @@ Your runtime beats 72.83 % of python3 submissions
 Your memory usage beats 64.78 % of python3 submissions (20 MB)
 """
 
-
+""" 
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         if not len(nums):
@@ -43,7 +43,7 @@ class Solution:
             return nums
 
         left, right = start, end
-        pivot = nums[int((start + end) / 2)]
+        pivot = nums[(start + end) // 2]
 
         while left <= right:
             while left <= right and nums[left] < pivot:
@@ -57,5 +57,91 @@ class Solution:
 
         self.quick_sort(nums, start, right)
         self.quick_sort(nums, left, end)
+"""
+
+
+""" 
+方法三：归并排序
+Accepted
+13/13 cases passed (500 ms)
+Your runtime beats 32.76 % of python3 submissions
+Your memory usage beats 87.44 % of python3 submissions (19.8 MB)
+"""
+
+
+""" 
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if not len(nums):
+            return nums
+
+        temp = [None] * len(nums)
+        return self.merge_sort(nums, 0, len(nums) - 1, temp)
+
+    def merge_sort(self, nums, start, end, temp):
+        if start >= end:
+            return nums
+
+        self.merge_sort(nums, start, (start + end) // 2, temp)
+        self.merge_sort(nums, (start + end) // 2 + 1, end, temp)
+        return self.merge(nums, start, end, temp)
+
+    def merge(self, nums, start, end, temp):
+        middle = (start + end) // 2
+        leftIndex = start
+        rightIndex = middle + 1
+        index = leftIndex
+
+        while leftIndex <= middle and rightIndex <= end:
+            if nums[leftIndex] < nums[rightIndex]:
+                temp[index] = nums[leftIndex]
+                index += 1
+                leftIndex += 1
+            else:
+                temp[index] = nums[rightIndex]
+                index += 1
+                rightIndex += 1
+
+        while leftIndex <= middle:
+            temp[index] = nums[leftIndex]
+            index += 1
+            leftIndex += 1
+
+        while rightIndex <= end:
+            temp[index] = nums[rightIndex]
+            index += 1
+            rightIndex += 1
+
+        for i in range(start, end + 1):
+            nums[i] = temp[i]
+
+        return nums
+"""
+
+""" 不同写法 """
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.merge_sort(nums, 0, len(nums) - 1)
+        return nums
+
+    def merge_sort(self, nums, left, right):
+        if left >= right:
+            return
+
+        middle = (left + right) // 2
+        self.merge_sort(nums, left, middle)
+        self.merge_sort(nums, middle + 1, right)
+        temp = []
+        i, j = left, middle + 1
+        while i <= middle or j <= right:
+            if i > middle or (j <= right and nums[j] < nums[i]):
+                temp.append(nums[j])
+                j += 1
+            else:
+                temp.append(nums[i])
+                i += 1
+        nums[left: right + 1] = temp
 
 # @lc code=end
